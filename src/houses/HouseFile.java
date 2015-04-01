86f345e0d724201ee7270039c8daea356a3b11d9
Eranda Jayanath <erandad89@gmail.com>
2015-04-01 10:20:48 +0530

    Update HouseFile.java

--- src/houses/HouseFile.java
+++ src/houses/HouseFile.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package houses;
 
 /**
  *
- * @author darsh
+ * @author Eranda
  */
 import java.io.*;
 
